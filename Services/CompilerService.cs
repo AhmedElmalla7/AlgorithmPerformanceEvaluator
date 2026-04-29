6df41a9b5e9cba678ad9dcb1b8fbfc5b3a5aeccb
AhmedElmalla7 <245457150+AhmedElmalla7@users.noreply.github.com>
2026-04-29 23:48:53 +0300

    implement class CompilerService

--- Services/CompilerService.cs
+++ Services/CompilerService.cs
@@ -1,10 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
 
 namespace AlgorithmPerformanceEvaluator.Services
 {
-    internal class CompilerService
+    public class CompilerService
     {
+        private static readonly ScriptOptions _opts = ScriptOptions.Default
+            .WithImports("System", "System.Linq", "System.Collections.Generic")
+            .WithReferences(typeof(object).Assembly);
+
+        public async Task<Func<int[], object?>> CompileAsync(string userCode)
+        {
+            if (string.IsNullOrWhiteSpace(userCode))
+                throw new ArgumentException("User code cannot be empty.");
+
+            string wrapped = $@"
+                using System;
+                using System.Linq;
+                using System.Collections.Generic;
+
+                Func<int[], object?> fn = (int[] arr) =>
+                {{
+                    {userCode}
+                }};
+                return fn;
+            ";
+
+            try
+            {
+                var fn = await CSharpScript
+                    .EvaluateAsync<Func<int[], object?>>(wrapped, _opts);
+
+                if (fn == null)
+                    throw new InvalidOperationException("Compilation succeeded but function is null.");
+
+                return fn;
+            }
+            catch (CompilationErrorException ex)
+            {
+                var errors = string.Join(Environment.NewLine, ex.Diagnostics);
+                throw new InvalidOperationException($"Compilation failed:{Environment.NewLine}{errors}");
+            }
+        }
     }
 }
