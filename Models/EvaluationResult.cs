4667cebf4c8e56163ff42e81ee105a5f0a3eefef
AhmedElmalla7 <245457150+AhmedElmalla7@users.noreply.github.com>
2026-04-29 23:17:24 +0300

    implement class EvaluationResult

--- Models/EvaluationResult.cs
+++ Models/EvaluationResult.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace AlgorithmPerformanceEvaluator.Models
 {
-    internal class EvaluationResult
+    public class EvaluationResult
     {
+        public List<int> InputSizes { get; set; } = new();
+        public List<double> AvgTimes { get; set; } = new();
+        public List<double> BestTimes { get; set; } = new();
+        public List<double> WorstTimes { get; set; } = new();
+        public string Complexity { get; set; } = "";
+        public string Description { get; set; } = "";
+        public double Confidence { get; set; }
     }
 }
