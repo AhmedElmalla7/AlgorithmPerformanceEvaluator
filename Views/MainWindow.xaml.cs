46200234bc9d83ab1f850337adedc9f2ce4ce5c5
hendawy <mohamedmajdy615@gmail.com>
2026-04-30 01:51:34 +0300

    add UI

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,15 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Windows;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Windows.Media;
 
 namespace AlgorithmPerformanceEvaluator
 {
     public partial class MainWindow : Window
     {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
 
+        private void btnManual_Click(object sender, RoutedEventArgs e)
+        {
+            manualInputPanel.Visibility = Visibility.Visible;
+        }
+
+        private void btnAuto_Click(object sender, RoutedEventArgs e)
+        {
+            manualInputPanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Run Clicked!");
+        }
     }
-}   
+}
