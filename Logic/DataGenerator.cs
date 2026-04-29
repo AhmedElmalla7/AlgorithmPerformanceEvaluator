0991fcb6f9ab45451f8b0989c9b3c4c9071d356d
AhmedElmalla7 <245457150+AhmedElmalla7@users.noreply.github.com>
2026-04-29 16:52:16 +0300

    initial commit

---
+++ Logic/DataGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmPerformanceEvaluator.Logic
+{
+    internal class DataGenerator
+    {
+    }
+}
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Windows;
+using System.Windows.Shapes;
+
+namespace AlgorithmPerformanceEvaluator
+{
+    public partial class MainWindow : Window
+    {
+
+    }
+}   
